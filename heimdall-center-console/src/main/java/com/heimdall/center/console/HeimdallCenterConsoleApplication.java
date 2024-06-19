1896030816897e5beb87a07be5be9ca216f1ddd0
maenping <maenping@yiruikecorp.com>
2024-06-19 17:09:37 +0800

    2

---
+++ heimdall-center-console/src/main/java/com/heimdall/center/console/HeimdallCenterConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.heimdall.center.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallCenterConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeimdallCenterConsoleApplication.class, args);
+	}
+
+}
---
+++ heimdall-center-core/src/test/java/com/heimdall/center/core/HeimdallCenterCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heimdall.center.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimdallCenterCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
